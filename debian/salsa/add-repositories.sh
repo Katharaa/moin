e7f5ea6736c6b2b613120633daf138acfe94e432
Paul Boddie <paul@boddie.org.uk>
2023-08-20 17:41:07 +0200

    Incorporated job details into the script.
    Since it is awkward to set up each environment so that additional
    data files might be made available, the job details have been
    incorporated into the script.

--- debian/salsa/add-repositories.sh
+++ debian/salsa/add-repositories.sh
@@ -3,11 +3,24 @@
 # Using a list of packages and associated jobs, define a configuration for apt
 # that allows the listed packages to be obtained for installation.
 #
-# Usage: add-dependencies.sh <apt base directory> <package job list file>
+# Usage: add-dependencies.sh <apt base directory>
 
-THISDIR=$(dirname "$0")
 APT_PATH=${1:-"/etc/apt"}
-SALSA_JOBS_LIST=${2:-"$THISDIR/salsa-jobs.txt"}
+
+# The jobs list would preferably be separate, but this requires extra effort
+# when setting up various environments.
+
+SALSA_JOBS_LIST='
+emeraldtree 4575438
+feedgen 4575509
+flask-theme 4575496
+flatland 4576442
+python-xstatic-autosize 4576472
+python-xstatic-bootstrap 4576577
+python-xstatic-ckeditor 4576527
+python-xstatic-jquery-file-upload 4576540
+python-xstatic-pygments 4576563
+'
 
 # Define repository location details for package dependencies.
 
@@ -17,7 +30,7 @@ PUBKEY_URL_SUFFIX="$APTLY_URL_SUFFIX/public-key.asc"
 
 # Add dependency details to an apt configuration in the indicated directory.
 #
-# Usage: add_dependencies <apt base directory> <package job list file>
+# Usage: add_dependencies <apt base directory>
 
 add_dependencies()
 {
@@ -32,14 +45,20 @@ add_dependencies()
         mkdir -p "$APT_GPG_TRUSTED_DIR"
     fi
 
-    # Add configuration entries and public keys.
+    # Extract the package and job identifier from the list.
 
-    while read PACKAGE JOBID ; do
+    echo "$SALSA_JOBS_LIST" | while read PACKAGE JOBID ; do
+        if [ ! "$PACKAGE" ] ; then
+            continue
+        fi
+
+        # Add configuration entries and public keys.
+
         cat >> "$APT_LIST" <<EOF
 deb ${TEAM_URL}/${PACKAGE}/-/jobs/${JOBID}/${APTLY_URL_SUFFIX} unstable main
 EOF
         get_public_key "$APT_GPG_TRUSTED_DIR" "$PACKAGE" "$JOBID"
-    done < "$2"
+    done
 }
 
 # Fetch the public key for a package, storing it in the indicated directory.
@@ -53,4 +72,4 @@ get_public_key()
 
 # Obtain the package and job details, adding dependency details.
 
-add_dependencies "$APT_PATH" "$SALSA_JOBS_LIST"
+add_dependencies "$APT_PATH"
