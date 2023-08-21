df02b8cabea5c90e5f6669cf051601bf65c45ef2
Paul Boddie <paul@boddie.org.uk>
2023-08-22 01:19:21 +0200

    Make scripts for dedicated purposes.

--- debian/salsa/add-repositories.sh
+++ debian/salsa/add-repositories.sh
@@ -1,39 +1,21 @@
 #!/bin/sh
 
-# Using a list of packages and associated jobs, define a configuration for apt
-# that allows the listed packages to be obtained for installation.
+# Using a list of packages, define a configuration for apt that allows the
+# listed packages to be obtained for installation.
 #
-# Usage: add-dependencies.sh <apt base directory>
+# Usage: add-repositories.sh <apt base directory>
+
+# Import common definitions.
+
+. $(dirname "$0")/repositories.sh
 
 APT_PATH=${1:-"/etc/apt"}
 
-# The packages list would preferably be separate, but this requires extra effort
-# when setting up various environments.
-
-SALSA_PACKAGES_LIST='
-emeraldtree
-feedgen
-flask-theme
-flatland
-python-xstatic-autosize
-python-xstatic-bootstrap
-python-xstatic-ckeditor
-python-xstatic-jquery-file-upload
-python-xstatic-pygments
-'
-
-# Define repository location details for package dependencies.
-
-TEAM_URL="https://salsa.debian.org/moin-team"
-BRANCH="debian/master"
-APTLY_URL_SUFFIX="-/jobs/artifacts/${BRANCH}/raw/aptly?job=aptly"
-PUBKEY_URL_SUFFIX="public-key.asc"
-
-# Add dependency details to an apt configuration in the indicated directory.
+# Add repository details to an apt configuration in the indicated directory.
 #
-# Usage: add_dependencies <apt base directory>
+# Usage: add_repositories <apt base directory>
 
-add_dependencies()
+add_repositories()
 {
     APT_SOURCES="$1/sources.list.d"
     APT_LIST="${APT_SOURCES}/new-packages-testing.list"
@@ -55,18 +37,15 @@ add_dependencies()
 
     for PACKAGE in $SALSA_PACKAGES_LIST ; do
 
-        # Handle redirects by obtaining the last URL fetched. This eliminates
-        # the need to enable redirects in apt.
-
-        START_URL="${TEAM_URL}/${PACKAGE}/${APTLY_URL_SUFFIX}"
-        URL=$(curl -s -L -I -w '%{url_effective}' -o /dev/null "$START_URL")
-
         # Define the repository location.
+
+        URL=$(repository_location "$PACKAGE")
+
         # Generate an obvious failure if there is no appropriate repository.
         # That will hopefully make troubleshooting easier when other jobs try
         # and fetch from this non-existent repository.
 
-        if [ "$URL" = "$START_URL" ] ; then
+        if [ ! "$?" ] ; then
             echo "FAIL! $PACKAGE was not built successfully." >> "$APT_LIST"
         else
             echo "deb ${URL} unstable main" >> "$APT_LIST"
@@ -84,9 +63,9 @@ add_dependencies()
 apt-get update
 NON_INTERACTIVE=1 apt-get install -y curl
 
-# Obtain the package and job details, adding dependency details.
+# Obtain the package and repository details.
 
-add_dependencies "$APT_PATH"
+add_repositories "$APT_PATH"
 
 # Update the repository records.
 
