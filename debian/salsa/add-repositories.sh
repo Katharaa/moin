53b46a9baac68e4180beedae8955c32f823f3445
Paul Boddie <paul@boddie.org.uk>
2023-08-22 01:33:52 +0200

    Revert script change due to tool limitations.
    Since the script is being passed all by itself to various jobs, any
    accompanying scripts cannot be specified for import within the
    script.

--- debian/salsa/add-repositories.sh
+++ debian/salsa/add-repositories.sh
@@ -5,11 +5,53 @@
 #
 # Usage: add-repositories.sh <apt base directory>
 
-# Import common definitions.
-
-. $(dirname "$0")/repositories.sh
-
 APT_PATH=${1:-"/etc/apt"}
+
+# The packages list would preferably be separate, but this requires extra effort
+# when setting up various environments.
+
+SALSA_PACKAGES_LIST='
+emeraldtree
+feedgen
+flask-theme
+flatland
+python-xstatic-autosize
+python-xstatic-bootstrap
+python-xstatic-ckeditor
+python-xstatic-jquery-file-upload
+python-xstatic-pygments
+'
+
+# Define repository location details for package dependencies.
+
+TEAM_URL="https://salsa.debian.org/moin-team"
+BRANCH="debian/master"
+APTLY_URL_SUFFIX="-/jobs/artifacts/${BRANCH}/raw/aptly?job=aptly"
+PUBKEY_URL_SUFFIX="public-key.asc"
+
+# Return the repository location for a given package.
+#
+# Usage: repository_location <package>
+
+repository_location()
+{
+    # Handle redirects by obtaining the last URL fetched. This eliminates
+    # the need to enable redirects in apt.
+
+    START_URL="${TEAM_URL}/${PACKAGE}/${APTLY_URL_SUFFIX}"
+
+    # Let curl output the location itself.
+
+    curl -s -L -I -w '%{url_effective}' -o /dev/null "$START_URL"
+
+    # Return success or failure.
+
+    if [ "$URL" = "$START_URL" ] ; then
+        return 1
+    else
+        return 0
+    fi
+}
 
 # Add repository details to an apt configuration in the indicated directory.
 #
